79e4ad5fe5458e726bc9e304e50fa90d7daab0c1
Abir <abir.pal899@gmail.com>
2021-01-23 10:43:41 +0530

    Updated Header

--- src/components/Header/data.js
+++ src/components/Header/data.js
@@ -3,11 +3,18 @@ import { duration } from "@material-ui/core";
 const data = {
   heading: {
     line1: "Hack the Moutains",
-    line2: " Build Projects. Grow Connections. Get results. "
+    line2: "The Best view comes after the Hardest climb."
   },
   version: "2.0",
-  timeheading: `XX April -xx April 2021`,
+  timeheading: `15 April 2021 -xx April 2021`,
   duration: "36",
-  subheading: `Hack The Mountains , As said the best view comes after the hardest climb . It is a 36 hour Virtual Hackathon where all developers , innovators and designers will collaborate to build something innovative , creative and out of the box. `
+  description: `Upholding the same spirit we
+  bring to you “Hack The Mountain 2.0", a 36 hour long virtual hackathon.
+  This virtual hackathon gives a special chance to all developers, designers, and 
+  innovators to collectively collaborate and come up with contemporary ideas and
+   victorious implementation. Hack The Mountain, gives you a chance to discover your 
+   potential and be the judge   of your own global standing. This hackathon is a major
+   push to your potential.
+  `
 };
 export default data;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,42 +1,76 @@
 import React from "react";
-import { Grid, Typography, Container, Button, Box } from "@material-ui/core";
+import { Grid, Typography, Container, Button, Hidden } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import { motion } from "framer-motion";
+import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 import variants from "../variants";
 import data from "./data";
+import BG from "./assets/bg.svg"
 // import MLH from "../Sponsors/assets/mlh.svg";
 import HTM from "./assets/htm.svg";
 const useStyles = makeStyles({
   container: {
-    backgroundColor: "#202136",
+    background: "#202136",
+    backgroundImage: `url(${BG})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: "300px 150px",
     color: "white",
-    paddingBottom: "22px",
-    paddingTop: "30px"
+    paddingBottom: "10%",
+    paddingTop: "30px",
+    height: "100%"
+  },
+  mobilecontainer: {
+    background: "#202136",
+    backgroundImage: `url(${BG})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: "-120px 70px",
+    color: "white",
+    paddingBottom: "30%",
+    paddingTop: "30px",
+    height: "100%"
   },
   desc: {
     lineHeight: "32px",
     paddingBottom: "22px"
   },
   left: {
-    textAlign: "left"
+    textAlign: "center"
   },
   button: {
     color: "white",
     background: "black",
     border: "1px solid white",
     zIndex: 0,
+    textDecoration: 'none',
     "&hover": {
       background: "white",
-      color: "black"
+      color: "white",
+      textDecoration: 'none',
     }
   },
   logo: {
-    marginTop: "62px"
+    marginTop: "10px"
   },
-  heading: { marginTop: "30px" }
+  heading: {
+    marginTop: "70px",
+    // textShadow: '0 0 20px #fff, 0 0 30px #CA484F, 0 0 40px #CA484F, 0 0 50px #CA484F, 0 0 60px #CA484F, 0 0 70px #CA484F, 0 0 80px #CA484F' 
+  },
+  highlight: {
+    textShadow: `0 0 20px #fff, 
+             0 0 30px #CA484F,
+             0 0 40px #CA484F,
+             0 0 50px #CA484F,
+             0 0 60px #CA484F,
+             0 0 70px #CA484F,
+             0 0 80px #CA484F`
+  }
 });
 
 const Header = () => {
+  const history = useHistory();
   const classes = useStyles();
   return (
     <motion.div
@@ -44,50 +78,101 @@ const Header = () => {
       variants={variants}
       transition={{ duration: 4 }}
     >
-      <Grid className={classes.container} container>
-        <Grid className={classes.left} item xs={12} sm={12} md={6} lg={6}>
+      <Hidden smDown>
+        <Grid className={classes.container} container>
           <Container>
-            <Typography
-              className={classes.heading}
-              gutterBottom={true}
-              variant="h1"
-            >
-              {data.heading.line1} {data.version}
-            </Typography>
-            <Typography gutterBottom={true} variant="h5">
-              "{data.heading.line2}"
-            </Typography>
-            <Typography gutterBottom={true} variant="h5">
-              <b>{data.timeheading}</b>
-              <br />
-              <br />
-            </Typography>
+            <Grid container>
 
-            <Typography className={classes.desc} variant="subtitle1">
-              {data.subheading}
-            </Typography>
-            <Typography className={classes.desc} variant="body1">
-              {/* <b>League Partner</b>
-              <br /> */}
-              {/* <img alt="htm" src={MLH} /> */}
-              {/* League Partner Image */}
-            </Typography>
+              <br /><br />
+              {/* Desktop View */}
 
-            <Button variant="outlined" className={classes.button}>
-              Participate
-            </Button>
+              <Grid containeritem xs={12} sm={12} md={7} lg={7}>
+                {/* Title */}
+                <Grid item xs={12} sm={12} md={12} lg={12}>
+                <Typography
+                  className={classes.heading}
+                  variant="h1"
+                >
+                  <b>{data.heading.line1} {data.version}</b>
+                </Typography>
+                </Grid>
+                {/* Moto */}
+                <Grid item xs={12} sm={12} md={12} lg={12}>
+                <Typography gutterBottom={true} variant="h5" >
+                  "{data.heading.line2}"
+              </Typography>
+              </Grid>
+                {/* Date */}
+                <Grid item xs={12} sm={12} md={12} lg={12}>
+                <Typography gutterBottom={true} variant="h5">
+                  <br /><b>{data.timeheading}</b>
+                </Typography>
+                </Grid>
+                {/* Description */}
+                <Grid item xs={12} sm={12} md={12} lg={12}>
+                <Typography className={classes.desc} variant="h6">
+                  <b> {data.description}</b><br /><br />
+                </Typography>
+                </Grid>
+                <Grid container item >
+                  {/* Participate Button */}
+                    <Grid item xs={12} sm={12} md={3} lg={3}>
+                      <Link to="/register">
+                        <Button variant="outlined" className={classes.button}>
+                          Pre Register
+                        </Button>
+                      </Link>
+                    </Grid>{} 
+                  {/* Brochure Button */}
+                    <Grid item xs={12} sm={12} md={3} lg={3} >
+                      <Link to="/register">
+                        <Button variant="outlined" className={classes.button}>
+                          Brochure
+                        </Button>
+                      </Link>
+                    </Grid><br /><br /><br /><br /><br /><br /><br />
+                  </Grid>
+              </Grid>
+
+
+
+            </Grid>
           </Container>
         </Grid>
-        <Grid item xs={12} sm={12} md={6} lg={6}>
-          <Box
-            className={classes.heading}
-            display="flex"
-            justifyContent="center"
-          >
-            <img src={HTM} alt="htm" className={classes.logo} width="70%" />
-          </Box>
-        </Grid>
-      </Grid>
+      </Hidden>
+      {/* Mobile View */}
+      <Hidden mdUp>
+        <Grid className={classes.mobilecontainer} container>
+          <Container>
+            <Grid container>
+
+              <br /><br />
+              {/* Title */}
+              <Typography
+
+                variant="h2"
+              >
+                <b>{data.heading.line1} {data.version}</b>
+              </Typography>
+              {/* Moto */}
+              <Typography gutterBottom={true} variant="h5" >
+                "{data.heading.line2}"
+              </Typography>
+              {/* Date */}
+              <Typography gutterBottom={true} variant="h5">
+                <b>{data.timeheading}</b>
+              </Typography>
+              {/* Participate Button */}
+              <Link to="/register">
+                <Button variant="outlined" className={classes.button}>
+                  Participate
+                </Button>
+              </Link>
+
+
+            </Grid>
+          </Container>
+        </Grid></Hidden>
     </motion.div>
   );
 };
