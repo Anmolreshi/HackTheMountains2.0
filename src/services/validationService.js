160a06d72751aac8596aece8bc6abe5f5f148376
ampsteric <ampsteric@gmail.com>
2021-01-31 16:16:35 +0530

    add all types of validation

--- src/services/validationService.js
+++ src/services/validationService.js
@@ -1,42 +1,27 @@
-/**
- *
- * Features Required ( MUST ) 
- * Logic
- * Step 1 : Identify the validation service
- * 1 for Email Validation
- * 2 for Name Validation ( cannot include numbers)
- * 3 for GitHub Link Validation, must start with https://github.com/
- * 4 for Resume Link Validation, must start with https://
- * 5 Mobile number validation ( 10 digits)
- * 6 Linkedin Profile Link validation, must have https://linkedin.com/in/
- * 7. Organization Name Validation ( It can have numbers in it, for example: Dare2C)
- * 
- * Step 2: Create a function, which returns true / false based on validation check.
- * Step 3: Function can use regex expressions for strict checking
- * This file is should be put in abstraction, the main logic validation code should be written in 
- * src/validators/
- * 
-    * For example *this* file can contain like this.
-    * 
-    * import {EmailValidation} from "../validators/emailValidationLogic.js"
-    * const ValidationService=(serviceno, data)=>{
-    * 
-    * if(service===1)
-    * {
-    *      return Emailvalidation(data)
-    * }
-    * 
-    * if(service===2)
-    * {
-    *      return NameValidation(data)
-    * }
-    * 
-    * }
-    * The main logic of the EmailValidation should be written in
-    *  (src/validators/emailValidationLogic.js)
-    * -------------------------------------------------------------
- * Developer Notes -- by Abir ( https://github.com/imabp)
- * These features cannot be neglected. And is an important measure for
- * maintianing data consistency in the servers.
- * 
- */
+import {EmailValidator , NameValidator, MobileValidator , resumeValidator , GithubValidator,LinkedInValidator} from '../validators/';
+
+export default function ValidationService(servicenumber , data){
+   switch(servicenumber){
+      case 0:
+         return EmailValidator(data);
+         break;
+      case 1:
+         return NameValidator(data);
+         break;
+      case 2:
+         return MobileValidator(data);
+         break;
+      case 3:
+         return resumeValidator(data);
+         break;
+      case 4:
+         return GithubValidator(data);
+         break;
+      case 5:
+         return LinkedInValidator(data);
+         break;
+
+   }
+   
+}
+
