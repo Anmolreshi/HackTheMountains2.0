0c9a26c045e239f194a86151dfc4798629b3d87f
Abir <abir.pal899@gmail.com>
2021-03-14 17:59:32 +0530

    Update GitHub Validation :)

--- src/validators/GithubValidator.js
+++ src/validators/GithubValidator.js
@@ -1,6 +1,6 @@
 export default function ValidateGithub(profile) 
 {
- if (/^(https?:\/\/)?(www\.)?github\.com\/[a-zA-Z0-9_]{1,25}$/igm .test(profile))
+ if (/^(https?:\/\/)?(www\.)?github\.com\/[a-zA-Z0-9_-]{1,25}$/igm .test(profile))
   {
     return (true)
   }
