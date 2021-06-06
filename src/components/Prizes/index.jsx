de6ca730ff68d4866e46272dd003cc7027ba9d1f
Abir <abir.pal899@gmail.com>
2021-06-06 20:11:15 +0530

    Updated Prize Section

--- src/components/Prizes/index.jsx
+++ src/components/Prizes/index.jsx
@@ -27,15 +27,10 @@ const useStyles = makeStyles({
     }
 
   },
-  highlighter: {
-    // textShadow: `0 0 20px aqua,
-    // 0 0 30px aqua,
-    // 0 0 40px aqua,
-    // 0 0 50px aqua,
-    // 0 0 60px aqua,
-    // 0 0 70px aqua,
-    // 0 0 80px aqua`
-  },
+  margins:{
+    marginLeft:'15px',
+    marginRight:'15px'
+  }
 });
 
 const Prizes = () => {
@@ -43,137 +38,78 @@ const Prizes = () => {
   const prize_data = usePrize();
   return (
     <div id="prizes" className={classes.container}>
-      <Grid container>
-        <Container>
-          <Typography
-            style={{ textAlign: "center" }}
-            className={classes.highlighter}
-
-            variant="h2"
-            gutterBottom={true}
-          >
-           <b>Prizes</b>
-          </Typography>
-
- <Grid container spacing={4}>
-            <Grid item xs={12} sm={12} md={12} lg={12} >
+      <div id="margins" className={classes.margins}>
+        <Grid container>
+          <Grid item xs={12} sm={12} md={12} lg={12}>
+            <Typography
+              style={{ textAlign: "center" }}
+              className={classes.highlighter}
+              variant="h2"
+              gutterBottom={true}
+            >
+              <b>Prizes</b>
               <br />
               <br />
-              <div style={{marginBottom:'10px',background: '#121229', padding:'20px', borderRadius:'10px',  lineHeight: '2'}}>
-              <Typography
-                style={{ textAlign: "center"}}
-
-                variant="h3"
-                gutterBottom={true}
+            </Typography>
+          </Grid>
+          <Grid container>
+            {/* Every Participant Left Side */}
+            <Grid container spacing={2} item xs={12} sm={12} md={4} lg={4}>
+              <div
+                style={{
+                  marginBottom: "10px",
+                  background: "#121229",
+                  padding: "20px",
+                  borderRadius: "10px",
+                  lineHeight: "2",
+                }}
               >
-                <b>Every Participant</b>
-              </Typography>
-              <Typography
-                style={{ textAlign: "center" }}
-                variant="body"
-                gutterBottom={true}
-              >
-                {prize_data.every_participant &&
-                  prize_data.every_participant.map((el) => (
-                    <>
-                      <li>{el}</li>
-
-                    </>
-                  ))}
-              </Typography>
-              <br />
-
+                <Typography
+                  style={{ textAlign: "center" }}
+                  variant="h3"
+                  gutterBottom={true}
+                >
+                  <b>Every Participant</b>
+                </Typography>
+                <Typography
+                  style={{ textAlign: "center" }}
+                  variant="body"
+                  gutterBottom={true}
+                >
+                  {prize_data.every_participant &&
+                    prize_data.every_participant.map((el) => (
+                      <>
+                        <li>{el}</li>
+                      </>
+                    ))}
+                </Typography>
               </div>
-
-              <br />
-              <Typography
-                style={{ textAlign: "center" }}
-                variant="h3"
-                gutterBottom={true}
-              >
-
-                <b>Special Prizes</b><br/>
-
-              </Typography>
-              <Grid container>
-                {prize_data.sponsor_prize &&
-                  prize_data.sponsor_prize.map((el) => (
-                    <>
-
-                      <Grid item xs={12} sm={12} md={12} lg={12} style={{marginBottom:'10px',background: '#121229', padding:'20px', borderRadius:'10px',  lineHeight: '2'}}>
-
-                        <Grid item xs={12} sm={12} md={12} lg={12}>
-                          <Typography
-                                 style={{ textAlign: "center" }}
-                            variant="h4"
-                            gutterBottom={true}
-                          >
-                            <b>{el.sponsor_name}</b>
-                          </Typography>
-
-                          <br />
-
-                        </Grid>
-                        <Grid item xs={12} sm={12} md={12} lg={12}>
-                          <Typography
-                            variant="body"     style={{ textAlign: "center" }}
-                            gutterBottom={true}
-                          >
-                            {el.description.map((prize) => (
-                              <>
-                                <li>{prize}</li>
-
-                                {/* <br /> */}
-
-                              </>
-                            ))}
-                          </Typography>
-                        </Grid>
-
-                        <Grid item xs={12} sm={12} md={12} lg={12} style={{textAlign:'center'}}>
-
-                          <Typography
-                            variant="body"      style={{ textAlign: "center" }}
-                            gutterBottom={true}
-                          >
-                            {el.notion_link && (
-                              <>
-
-                                <br />
-                                
-                                  <a href={el.notion_link}>
-                                  <Button variant="outlined" className={classes.button} >
-                                        Know More
-                                  </Button>
-                                  </a>
-                               
-                                <br />
-                              </>
-                            )}
-                          </Typography>
-                        </Grid>
-                      </Grid>
-                    </>
-                  ))}
-              </Grid>
-              <br />
-              <br />
-              <Typography
-                style={{ textAlign: "center" }}
-                variant="h3"
-                gutterBottom={true}
-              >
-                <b>HTM Prizes</b><br/>
-              </Typography>
-              <Grid container>
-                {prize_data.htm_prize &&
+            </Grid>
+            {/* Right Panel */}
+            <Grid container item xs={12} sm={12} md={8} lg={8}>
+              <Grid container  spacing={2}>
+              {prize_data.htm_prize &&
                   prize_data.htm_prize.map((el) => (
                     <>
-
-                      <Grid item xs={12} sm={12} md={12} lg={12} style={{marginBottom:'10px',background: '#121229', padding:'20px', borderRadius:'10px',  lineHeight: '3'}}>
-
+                      <Grid
+                      
+                        item
+                        xs={12}
+                        sm={12}
+                        md={4}
+                        lg={4}
+                        style={{
+                          marginBottom: "10px",
+                      
+                          background: "#121229",
+                          padding: "20px",
+                          borderRadius: "10px",
+                          lineHeight: "3",
+                        }}
+                      >
                         <Grid item xs={12} sm={12} md={12} lg={12}>
-                          <Typography      style={{ textAlign: "center" }}
+                          <Typography
+                            style={{ textAlign: "center" }}
                             variant="h4"
                             gutterBottom={true}
                           >
@@ -181,7 +117,8 @@ const Prizes = () => {
                           </Typography>
                         </Grid>
                         <Grid item xs={12} sm={12} md={12} lg={12}>
-                          <Typography      style={{ textAlign: "center" }}
+                          <Typography
+                            style={{ textAlign: "center" }}
                             variant="body"
                             gutterBottom={true}
                           >
@@ -189,9 +126,6 @@ const Prizes = () => {
                               {el.description.map((prize) => (
                                 <>
                                   <li>{prize}</li>
-
-                                  
-
                                 </>
                               ))}
                             </b>
@@ -200,18 +134,94 @@ const Prizes = () => {
                       </Grid>
                     </>
                   ))}
+                {prize_data.sponsor_prize &&
+                  prize_data.sponsor_prize.map((el) => (
+                    <>
+                      <Grid
+                        item
+                        xs={12}
+                        sm={12}
+                        md={4}
+                        lg={4}
+                        style={{
+                          marginBottom: "10px",
+                          background: "#121229",
+                          padding: "20px",
+                          borderRadius: "10px",
+                          lineHeight: "2",
+                        }}
+                      >
+                        <Grid item xs={12} sm={12} md={12} lg={12}>
+                          <Typography
+                            style={{ textAlign: "center" }}
+                            variant="h4"
+                            gutterBottom={true}
+                          >
+                            <b>{el.sponsor_name}</b>
+                          </Typography>
+
+                          <br />
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={12} lg={12}>
+                          <Typography
+                            variant="body"
+                            style={{ textAlign: "center" }}
+                            gutterBottom={true}
+                          >
+                            {el.description.map((prize) => (
+                              <>
+                                <li>{prize}</li>
+
+                                {/* <br /> */}
+                              </>
+                            ))}
+                          </Typography>
+                        </Grid>
+
+                        <Grid
+                          item
+                          xs={12}
+                          sm={12}
+                          md={12}
+                          lg={12}
+                          style={{ textAlign: "center" }}
+                        >
+                          <Typography
+                            variant="body"
+                            style={{ textAlign: "center" }}
+                            gutterBottom={true}
+                          >
+                            {el.notion_link && (
+                              <>
+                                <br />
+
+                                <a href={el.notion_link}>
+                                  <Button
+                                    variant="outlined"
+                                    className={classes.button}
+                                  >
+                                    Know More
+                                  </Button>
+                                </a>
+
+                                <br />
+                              </>
+                            )}
+                          </Typography>
+                        </Grid>
+                      </Grid>
+                    </>
+                  ))}
+                 
               </Grid>
-              <br />
-
             </Grid>
-          </Grid>
-         
 
-        </Container>
-      </Grid>
+           
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 };
 
 export default Prizes;
-
