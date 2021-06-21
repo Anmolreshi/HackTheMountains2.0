3b6acefd90a5f0d0394240b99ace55e12182a5bf
Lakshay <lakshaysinghal08@gmail.com>
2021-06-22 03:02:37 +0530

    Updated Schedule Page
    Signed-off-by: Lakshay <lakshaysinghal08@gmail.com>

--- src/components/Schedule/index.jsx
+++ src/components/Schedule/index.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import {Container, Grid, Typography} from "@material-ui/core"
-import Timeline from "./components/timeline"
+import {Container, Grid, Typography} from "@material-ui/core";
+import sc1 from "./assets/1.png";
+import sc2 from "./assets/2.png";
+import sc3 from "./assets/3.png";
+// import Timeline from "./components/timeline"
 export default function AlternateTimeline() {
   return (<>
-    <br/><br/><br/><br/>
-    <Container>
+    <br/><br/><br/><br/><br/><br/>
+    {/* <Container>
     <Grid container spacing={5} > <Grid item xs={12} sm={12} md={4} lg={4}>
-      <Timeline/>
+       <Timeline/> 
       </Grid>
       <Grid item xs={12} sm={12} md={4} lg={4}>
       <Typography gutterBottom={true}>Being on time, is a good thing. We at Hack the Mountains, try to be best on schedule. Here is one to help you out in this hackathon</Typography>
@@ -15,7 +18,30 @@ export default function AlternateTimeline() {
       </Typography>
       </Grid>
    
-      </Grid></Container>
+      </Grid></Container> */}
+
+
+      <Container spacing={5}>
+        <Grid item xs={12} sm={12} md={5} lg={5} style={{justifyItems:'center', margin:'auto'}}>
+        <Typography gutterBottom={true}>
+            Being on time, is a good thing. We at Hack the Mountains, try to be best on schedule. Here is one to help you out in this hackathon
+        </Typography>
+
+        <br/><br/>
+
+        </Grid>
+
+        <Grid item xs={12} sm={12} md={6} lg={6} style={{justifyItems:'center', margin:'auto'}}>
+        <img src={sc1} width="100%"/>
+        <img src={sc2} width="100%"/>
+        <img src={sc3} width="100%"/>
+
+        <br/><br/><br/><br/>
+        </Grid>
+
+      </Container>
+
+
     </>
   );
 }
